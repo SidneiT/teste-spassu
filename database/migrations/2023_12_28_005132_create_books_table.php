d5206541514f8c7318e58eba7d2903c8ece9c88b
Sidnei Teixeira <sidnei.teixeira@outlook.com.br>
2023-12-27 23:14:52 -0300

    chore: create migration files and resources

--- database/migrations/2023_12_28_005132_create_books_table.php
+++ database/migrations/2023_12_28_005132_create_books_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('books', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title', 40);
+            $table->string('publisher', 40);
+            $table->integer('edition');
+            $table->integer('year');
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('books');
     }
 };
--- database/migrations/2023_12_28_015158_create_book_topic.php
+++ database/migrations/2023_12_28_015158_create_book_topic.php
@@ -11,10 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('book_topic', function (Blueprint $table) {
+            $table->foreignId('book_id')->constrained('books');
+            $table->foreignId('topic_id')->constrained('topics');
         });
     }
 
@@ -23,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('book_topic');
     }
 };
